d20f1da566d9bee97fd861456bce6fab9ae814d2
Andrey <andrtaw1@gmail.com>
2019-04-16 14:14:21 +0300

    feat: add footer

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,5 +1,68 @@
 <template>
-  <div>
-    <h1>Footer</h1>
-  </div>
+  <footer class="footer">
+    <StartMining/>
+    <div class="footer__nav">
+      <div v-for="nav in navigation" :key="nav.title" class="footer__nav-item">
+        <div class="footer__nav-title">{{nav.title}}</div>
+        <a
+          v-for="link in nav.links"
+          :key="link.title"
+          class="footer__nav-link"
+          :href="link.url"
+          target="_blank"
+        >{{link.title}}</a>
+      </div>
+    </div>
+    <div class="footer__bottom">
+      <div class="footer__bottom-item">© NKN Explorer 2019</div>
+    </div>
+  </footer>
 </template>
+
+<style lang="scss">
+@import './Footer';
+</style>
+
+
+<script>
+import StartMining from '~/components/StartMining/StartMining'
+
+export default {
+  components: {
+    StartMining
+  },
+
+  data: () => {
+    return {
+      navigation: [
+        {
+          title: 'Company',
+          links: [
+            {
+              title: 'About',
+              url: 'https://www.nkn.org/#/about'
+            },
+            {
+              title: 'Official Website',
+              url: 'https://www.nkn.org/'
+            }
+          ]
+        },
+        {
+          title: 'Community',
+          links: [
+            {
+              title: 'NKNx Node Manager',
+              url: 'https://www.nknx.org'
+            },
+            {
+              title: 'NKN Wallet',
+              url: 'https://nknwallet.io'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ components/StartMining/StartMining.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="start-mining">
+    <div class="start-mining__icon">
+      <Mining/>
+    </div>
+    <h4 class="start-mining__nodes">{{networkNodes}} {{$t('currentNodes')}}</h4>
+    <div class="start-mining__text">{{$t('wantToJoinNKN')}}</div>
+    <Button
+      class="start-mining__button"
+      theme="dark-blue"
+      url="https://nknx.org/"
+      type="link"
+    >{{$t('startMining')}}</Button>
+  </div>
+</template>
+
+<style lang="scss">
+@import './StartMining';
+</style>
+
+<script>
+import Button from '~/components/Button/Button'
+
+import Mining from '~/assets/icons/mining.svg'
+
+export default {
+  components: { Button, Mining },
+  data: () => {
+    return {
+      networkNodes: 10000
+    }
+  },
+  mounted: function() {},
+  methods: {}
+}
+</script>
