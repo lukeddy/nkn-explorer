bf40204809f9b8ae9880326e588c3bab90c552e1
Andrey <andrtaw1@gmail.com>
2019-04-23 23:17:46 +0300

    fix: market stats, amount rounding

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 
-Vue.filter('commaNumber', function (x) {
-  return (x).toString().replace(
-    /(\d)(?=(?:\d{3})+(?:\.|$))|(\.\d*)$/g,
-    function (m, s1, s2) {
-      return s2 || (s1 + ',');
-    }
-  );
+Vue.filter('commaNumber', function(x) {
+  return x
+    .toString()
+    .replace(/(\d)(?=(?:\d{3})+(?:\.|$))|(\.\d*)$/g, function(m, s1, s2) {
+      return s2 || s1 + ','
+    })
 })
 
-Vue.filter('nknValue', function (x) {
-  return (x / 100000000).toFixed(2)
+Vue.filter('nknValue', function(x) {
+  return x / 100000000
 })
