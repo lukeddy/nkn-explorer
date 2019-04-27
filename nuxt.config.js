4cc2b6ba3fb2a2ae93e71a4962eac0940ebb3f7d
Christian Busch <kontakt@christian-busch.com>
2019-04-27 20:23:50 +0200

    feat: mobile icons, favicons and OG-descriptions

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,11 +10,22 @@ export default {
     title: pkg.name,
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'msapplication-TileColor', content: '#0b163c' },
+      { name: 'theme-color', content: '#ffffff' },
+      { property: 'og:title', content: 'NKN Explorer' },
+      { property: 'og:description', content: 'With our NKN explorer you can easily browse the whole blockchain of the NKN Network.' },
+      { property: 'og:image', content: '/thumbnail.jpg' },
+      { property: 'og:url', content: '/' },
+      { name: 'twitter:card', content: 'summary_large_image' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
       {
         rel: 'stylesheet',
         href: 'https://use.fontawesome.com/releases/v5.8.1/css/all.css'
