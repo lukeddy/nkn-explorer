b5122afaf700a3eb6a0f9ca1868b2b5b28ec17b2
Andrey <andrtaw1@gmail.com>
2019-07-02 18:17:39 +0300

    feat: add ip-check styles

--- pages/ips/index.vue
+++ pages/ips/index.vue
@@ -9,7 +9,7 @@
               <h6
                 class="page-header__stats-title text_color_white text_transform_uppercase text_opacity_75"
               >{{$t('your_node_status')}}</h6>
-              <h4 v-if="!response" class="page-header__stats-value text_color_white">INPUT A IP</h4>
+              <h4 v-if="!response" class="page-header__stats-value text_color_white">INPUT AN IP</h4>
               <h4
                 v-else-if="response.syncState == 'OFFLINE'"
                 class="page-header__stats-value text_color_white"
@@ -21,49 +21,52 @@
         <div class="page-header__right"></div>
       </div>
     </div>
-
-    <mq-layout mq="lg">
-      <div>
-        <h2>How does it work?</h2>
-        <p>
-          Checking your node is pretty easy: just put your node-ip into the corresponding input fields and click on "check my node!".
-          <br />You will see the results instantly on the right side.
-        </p>
+    <CardContainer>
+      <Card :width="$mq === 'lg' && response !== false ? 'half' : 'full'">
+        <h1 class="ip__title">How does it work?</h1>
+        <p
+          class="ip__desc"
+        >Checking your node is pretty easy: just put your node-ip into the corresponding input fields and click on "check my node!". You will see the results instantly on the right side.</p>
         <div class="main-header__search-bar">
-          <div class="search main-header__search search_type_">
+          <div class="search main-header__search ip__search">
             <input
+              v-model="ip"
               type="text"
               placeholder="Your node IP"
               class="search__control"
-              v-model="ip"
               @keyup.enter="checkNode()"
             />
             <span class="search__icon fe fe-search"></span>
           </div>
-          <button
-            class="button main-header__search-button button_theme_primary button_full"
-            @click="checkNode()"
-          >{{$t('check_my_node')}}</button>
+          <Button
+            theme="primary"
+            type="button"
+            :full="$mq === 'lg' ? false : true"
+            @click.native="checkNode()"
+          >{{$t('check_my_node')}}</Button>
         </div>
-      </div>
-      <div>
+      </Card>
+      <Card v-if="response !== false" :width="$mq === 'lg' ? 'half' : 'full'">
         <vue-code-highlight>{{response}}</vue-code-highlight>
-      </div>
-    </mq-layout>
+      </Card>
+    </CardContainer>
   </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import { component as vueCodeHighlight } from 'vue-code-highlight'
+import CardContainer from '~/components/CardContainer/CardContainer'
+import Card from '~/components/Card/Card'
+import Button from '~/components/Button/Button'
 
 export default {
   transition: 'expand',
-  components: { vueCodeHighlight },
+  components: { vueCodeHighlight, CardContainer, Card, Button },
   data: () => {
     return {
       ip: '',
-      response: ''
+      response: false
     }
   },
   computed: mapGetters({}),
