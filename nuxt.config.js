c8506407ce84249320a797ec342bd1755f41d616
Christian Busch <kontakt@christian-busch.com>
2019-08-05 15:40:54 +0200

    fix: SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       {
+        name: 'content-type',
+        content:
+          'text/html'
+      },
+      {
         name: 'viewport',
         content:
           'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
